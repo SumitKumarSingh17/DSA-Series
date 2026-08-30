d8ee84eaf13cbc10aaea6604692fc447e3274012
Sumit Kumar Singh <sumitkrsingh885@gmail.com>
2026-08-30 11:29:14 +0530

    Time: 7 ms (5.35%), Space: 9.7 MB (94.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // Initialize two pointers: left starts from beginning, right from end
-        int left = 0;
-        int right = s.size() - 1;
-      
-        // Continue checking while pointers haven't crossed
-        while (left < right) {
-            // Skip non-alphanumeric character from the left
-            if (!isalnum(s[left])) {
-                ++left;
-            }
-            // Skip non-alphanumeric character from the right
-            else if (!isalnum(s[right])) {
-                --right;
-            }
-            // Compare characters (case-insensitive)
-            // If they don't match, it's not a palindrome
-            else if (tolower(s[left]) != tolower(s[right])) {
-                return false;
-            }
-            // Characters match, move both pointers inward
-            else {
-                ++left;
-                --right;
+        int n=s.size()-1;
+        int l=0, r=n;
+        while(l<r){
+            if(!isalnum(s[l])) l++;
+            else if(!isalnum(s[r])) r--;
+            else if(tolower(s[l])!=tolower(s[r])) return false;
+            else{
+                l++;
+                r--;
             }
         }
-      
-        // All alphanumeric characters matched successfully
         return true;
     }
 };
