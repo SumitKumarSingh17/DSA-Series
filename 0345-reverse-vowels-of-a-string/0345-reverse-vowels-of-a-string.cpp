f1c1075656607975b86fcfbfeae4c4d6fe807d92
Sumit Kumar Singh <sumitkrsingh885@gmail.com>
2026-09-24 10:05:31 +0530

    Time: 0 ms (100%), Space: 10.2 MB (92.16%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
+    bool lower(char c){
+        if(tolower(c)=='a' || tolower(c)=='e' || tolower(c)=='i' || tolower(c)=='o' || tolower(c)=='u') return true;
+        return false;
+    }
     string reverseVowels(string s) {
-        string v="";
-        for(int i=0; i<s.size(); i++){
-            if(tolower(s[i])=='a' || tolower(s[i])=='e' || tolower(s[i])=='i' || tolower(s[i])=='o' || tolower(s[i])=='u'){
-                v+=s[i];
-            }
-        }
-        reverse(v.begin(), v.end());
-        int j=0;
-        for(int i=0; i<s.size(); i++){
-            if(tolower(s[i])=='a' || tolower(s[i])=='e' || tolower(s[i])=='i' || tolower(s[i])=='o' || tolower(s[i])=='u'){
-                s[i]=v[j];
-                j++;
-            }
+        int i=0, j=s.size()-1;
+        while(i<j){
+            while(i<j && !lower(s[i])) i++;
+            while(i<j && !lower(s[j])) j--;
+            swap(s[i],s[j]);
+            i++;
+            j--;
         }
         return s;
     }
